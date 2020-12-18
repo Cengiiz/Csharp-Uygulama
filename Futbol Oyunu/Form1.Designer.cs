bab62660a565c88b023220d1f0064dcfb1fc5ddc
Cengizhan KOÇAL <cngzkcl53@gmail.com>
2020-12-18 21:03:03 +0300

    Initial commit

---
+++ Futbol Oyunu/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Medyaplayerkullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-1, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanici Adi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(117, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sifre:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(219, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(219, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 30);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(283, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Giris";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(82, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(248, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lig Oyununa Hosgeldiniz :)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(436, 383);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Futbol Oyunu/Form2.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medyaplayerkullanimi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Random rastgele = new Random();
+        int gspuan = 0;
+        int fbpuan = 0;
+        int tspuan = 0; 
+        int bjkpuan = 0;
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            int a = rastgele.Next(0, 11);
+            int b = rastgele.Next(0, 11);
+            int c = rastgele.Next(0, 11);
+            int d = rastgele.Next(0, 11);
+            label4.Text = a.ToString();
+            label5.Text = b.ToString();
+            label6.Text = c.ToString();
+            label7.Text = d.ToString();
+            if (a > b)
+            {
+                gspuan = gspuan + 3;
+                labelgspuan.Text = gspuan.ToString();
+            }
+            else if (b>a)
+            {
+                fbpuan = fbpuan + 3;
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+            else
+            {
+                fbpuan = fbpuan + 1;
+                gspuan = gspuan + 1;
+                labelgspuan.Text = gspuan.ToString();
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+
+            if (c > d)
+            {
+                tspuan = tspuan + 3;
+                labeltspuan.Text = tspuan.ToString();
+            }
+            else if (d > c)
+            {
+                bjkpuan = bjkpuan + 3;
+                labelbjkpuan.Text = bjkpuan.ToString();
+            }
+            else
+            {
+                tspuan = tspuan + 1;
+                bjkpuan = bjkpuan + 1;
+                labeltspuan.Text = tspuan.ToString();
+                labelbjkpuan.Text = bjkpuan.ToString();
+            }
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            int a = rastgele.Next(0, 11);
+            int b = rastgele.Next(0, 11);
+            int c = rastgele.Next(0, 11);
+            int d = rastgele.Next(0, 11);
+
+            label21.Text = a.ToString();
+            label18.Text = b.ToString();
+            label12.Text = d.ToString();
+            label11.Text = c.ToString();
+            if (a > b)
+            {
+                gspuan = gspuan + 3;
+                labelgspuan.Text = gspuan.ToString();
+            }
+            else if (b > a)
+            {
+                bjkpuan = bjkpuan + 3;
+                labelbjkpuan.Text = bjkpuan.ToString();
+            }
+            else
+            {
+                bjkpuan = bjkpuan + 1;
+                gspuan = gspuan + 1;
+                labelgspuan.Text = gspuan.ToString();
+                labelbjkpuan.Text = bjkpuan.ToString();
+            }
+
+            if (c > d)
+            {
+                tspuan = tspuan + 3;
+                labeltspuan.Text = tspuan.ToString();
+            }
+            else if (d > c)
+            {
+                fbpuan = fbpuan + 3;
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+            else
+            {
+                tspuan = tspuan + 1;
+                fbpuan = fbpuan + 1;
+                labeltspuan.Text = tspuan.ToString();
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button4.Visible = true;
+            int a = rastgele.Next(0, 11);
+            int b = rastgele.Next(0, 11);
+            int c = rastgele.Next(0, 11);
+            int d = rastgele.Next(0, 11);
+
+            label31.Text = a.ToString();
+            label30.Text = b.ToString();
+            label26.Text = d.ToString();
+            label25.Text = c.ToString();
+            if (a > b)
+            {
+                gspuan = gspuan + 3;
+                labelgspuan.Text = gspuan.ToString();
+            }
+            else if (b > a)
+            {
+                tspuan = tspuan + 3;
+                labeltspuan.Text = tspuan.ToString();
+            }
+            else
+            {
+                tspuan = tspuan + 1;
+                gspuan = gspuan + 1;
+                labelgspuan.Text = gspuan.ToString();
+                labeltspuan.Text = tspuan.ToString();
+            }
+
+            if (c > d)
+            {
+                bjkpuan = bjkpuan + 3;
+                labelbjkpuan.Text = bjkpuan.ToString();
+            }
+            else if (d > c)
+            {
+                fbpuan = fbpuan + 3;
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+            else
+            {
+                bjkpuan = bjkpuan + 1;
+                fbpuan = fbpuan + 1;
+                labelbjkpuan.Text = bjkpuan.ToString();
+                labelfbpuan.Text = fbpuan.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(labelgspuan.Text) > Convert.ToInt32(labelfbpuan.Text)&& Convert.ToInt32(labelgspuan.Text) > Convert.ToInt32(labeltspuan.Text)&& Convert.ToInt32(labelgspuan.Text) > Convert.ToInt32(labelbjkpuan.Text))
+            {
+                Sampiyonluk.Visible = true;
+                Sampiyonluk.Text = "Sampiyonluk Galatasaray";
+                axWindowsMediaPlayer1.URL = "C:\\Oyun\\Futbol Oyunu\\bin\\muzik\\gs.mp3"; 
+                pictureBox1.ImageLocation = "C:\\Oyun\\Futbol Oyunu\\bin\\resim\\gs.jpg";
+            }
+            else if (Convert.ToInt32(labeltspuan.Text) > Convert.ToInt32(labelfbpuan.Text) && Convert.ToInt32(labeltspuan.Text) > Convert.ToInt32(labelgspuan.Text) && Convert.ToInt32(labeltspuan.Text) > Convert.ToInt32(labelbjkpuan.Text))
+            {
+                Sampiyonluk.Visible = true;
+                Sampiyonluk.Text = "Sampiyonluk Trabzonspor";
+                axWindowsMediaPlayer1.URL = "C:\\Oyun\\Futbol Oyunu\\bin\\muzik\\ts.mp3";
+                pictureBox1.ImageLocation = "C:\\Oyun\\Futbol Oyunu\\bin\\resim\\ts.jpg";
+            }
+            else if (Convert.ToInt32(labelbjkpuan.Text) > Convert.ToInt32(labelfbpuan.Text) && Convert.ToInt32(labelbjkpuan.Text) > Convert.ToInt32(labelgspuan.Text) && Convert.ToInt32(labelbjkpuan.Text) > Convert.ToInt32(labeltspuan.Text))
+            {
+                Sampiyonluk.Visible = true;
+                Sampiyonluk.Text = "Sampiyonluk Besiktas";
+                axWindowsMediaPlayer1.URL = "C:\\Oyun\\Futbol Oyunu\\bin\\muzik\\bjk.mp3";
+                pictureBox1.ImageLocation = "C:\\Oyun\\Futbol Oyunu\\bin\\resim\\bjk.jpg";
+            }
+            else if(Convert.ToInt32(labelfbpuan.Text) > Convert.ToInt32(labelbjkpuan.Text) && Convert.ToInt32(labelfbpuan.Text) > Convert.ToInt32(labelgspuan.Text) && Convert.ToInt32(labelfbpuan.Text) > Convert.ToInt32(labeltspuan.Text))
+            {
+                Sampiyonluk.Visible = true;
+                Sampiyonluk.Text = "Sampiyonluk Fenerbahce";
+                axWindowsMediaPlayer1.URL = "C:\\Oyun\\Futbol Oyunu\\bin\\muzik\\fb.mp3";
+                pictureBox1.ImageLocation = "C:\\Oyun\\Futbol Oyunu\\bin\\resim\\fb.jpg";
+            }
+        }
+    }
+}
